5a589e52c04962abf86620c0b3f874071d218459
adamvozda <adamvozda88@seznam.cz>
2022-03-02 09:46:31 +0100

    c

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true
 
 app.get("/", (req, res)=>{
 <<<<<<< HEAD
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  
 =======
 >>>>>>> 33ffc2ae1558f3fd55eb4120e33560325fd9bece
     res.send("Zdarec");

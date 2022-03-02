4da989488b1a042a1ee0a087086e5dea05417487
adamvozda <adamvozda88@seznam.cz>
2022-03-02 09:05:32 +0100

    make it better

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
    port: process.env.PORT,
-   mongoURI: process.env.MONGO_URI,
+   mongoURI: "mongodb+srv://Vozzda:5fe9ddc4@cluster0.p83z3.mongodb.net/Miffed?retryWrites=true&w=majority",
 };
+
+//process.env.MONGO_URI

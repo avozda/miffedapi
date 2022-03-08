e9332b9847ed1525ba9e4d03d91a58ce268ca3a9
adamvozda <adamvozda88@seznam.cz>
2022-03-08 21:33:19 +0100

    sds

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var cors = require('cors');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors({origin: 'http://localhost:8080'}));
 
-
+app.use(express.json({ extended: false }))
+app.use(bodyParser.json({ extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
